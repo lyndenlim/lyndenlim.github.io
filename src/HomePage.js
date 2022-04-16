3ee527a9e942323cb1b93bf2071c68ee3079e1d7
lynden <lyndenlim15@gmail.com>
2022-04-16 17:39:45 -0400

    Add buttons and links

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 function HomePage() {
     return (
         <div>
             Lynden Lim
             <br />
-            Button links here
+            <Link to="/about">
+                <button>About</button>
+            </Link>
+            <Link to="/projects">
+                <button>Projects</button>
+            </Link>
+            <Link to="/skills">
+                <button>Skills</button>
+            </Link>
+            <Link to="/contact">
+                <button>Contact</button>
+            </Link>
         </div>
     )
 }
