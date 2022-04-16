a28bb30515a8846352b0a59ec76fc7c96c410257
lynden <lyndenlim15@gmail.com>
2022-04-16 18:44:13 -0400

    Build skills page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "./HomePage"
 import About from "./About"
 import Projects from "./Projects"
+import Skills from "./Skills"
 import Contact from "./Contact"
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects/>} />
+        <Route path="/skills" element={<Skills/>} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
--- src/Skills.js
+++ src/Skills.js
@@ -1,9 +1,59 @@
 import React from 'react'
+import react from "./images/react.png"
+import python from "./images/python.png"
+import ruby from "./images/ruby.png"
+import javascript from "./images/javascript.png"
+import postgresql from "./images/postgresql.png"
+import git from "./images/git.png"
+import html from "./images/html.png"
+import css from "./images/css.png"
 
 function Skills() {
-  return (
-    <div>Skills</div>
-  )
+    return (
+        <div className="skill-container">
+            <div className="skill-image-container">
+                <img className="skill-image" src={react} alt="react" />
+                <br />
+                <span className="skill">React</span>
+            </div>
+            <div className="skill-image-container">
+                <img className="skill-image" src={python} alt="python" />
+                <br />
+                <span className="skill">Python</span>
+            </div>
+            <div className="skill-image-container">
+                <img className="skill-image" src={ruby} alt="ruby" />
+                <br />
+                <span className="skill">Ruby</span>
+            </div>
+            <div className="skill-image-container">
+                <img className="skill-image" src={postgresql} alt="postgresql" />
+                <br />
+                <span className="skill">PostgreSQL</span>
+            </div>
+            <div className="skill-image-container">
+                <img className="skill-image" src={git} alt="git" />
+                <br />
+                <span className="skill">GIT</span>
+            </div>
+            <div className="skill-image-container">
+                <img className="skill-image" src={javascript} alt="javascript" />
+                <br />
+                <span className="skill">JavaScript</span>
+            </div>
+            <div className="skill-image-container">
+                <img className="skill-image" src={html} alt="html" />
+                <br />
+                <span className="skill">HTML</span>
+            </div>
+            <div className="skill-image-container">
+                <img className="skill-image" src={css} alt="css" />
+                <br />
+                <span className="skill">CSS</span>
+            </div>
+        </div >
+
+    )
 }
 
 export default Skills
