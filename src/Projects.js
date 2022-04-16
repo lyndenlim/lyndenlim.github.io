ae76d6f7f107f21190ffc410bdff4417847df44e
lynden <lyndenlim15@gmail.com>
2022-04-16 18:00:07 -0400

    Build projects page

--- src/Projects.js
+++ src/Projects.js
@@ -1,9 +1,38 @@
 import React from 'react'
 
 function Projects() {
-  return (
-    <div>Projects</div>
-  )
+    return (
+        <div>
+            Projects
+            <div>
+                <h3>Undecided</h3>
+                <p>Img here</p>
+                <p>Restaurant/recipe assistant with randomizer feature that returns a restaurant within a mile radius of your current location.</p>
+                <p>React, Ruby on Rails, PostgreSQL, JavaScript, HTML/CSS</p>
+                <a href="https://github.com/lyndenlim/undecided" target="_blank">GitHub</a>
+                &nbsp;
+                <a href="https://undecided-application.herokuapp.com/">Website</a>
+            </div>
+            <div>
+                <h3>Schema</h3>
+                <p>Img here</p>
+                <p>Video viewing and streaming platform that centralizes topics in technology, programming, and science.</p>
+                <p>React, Ruby on Rails, PostgreSQL, JavaScript, HTML/CSS/SASS</p>
+                <a href="https://github.com/lyndenlim/schema" target="_blank">GitHub</a>
+                &nbsp;
+                <a href="https://schema-project.herokuapp.com/">Website</a>
+            </div>
+            <div>
+            <h3>Hurdle</h3>
+                <p>Img here</p>
+                <p>Inspired by WORDLE. Guess the word in 6 tries, dictionary entry of word appears after game concludes.</p>
+                <p>React, JavaScript, HTML/CSS</p>
+                <a href="https://github.com/lyndenlim/hurdle" target="_blank">GitHub</a>
+                &nbsp;
+                <a href="https://lyndenlim.github.io/hurdle/">Website</a>
+            </div>
+        </div>
+    )
 }
 
 export default Projects
